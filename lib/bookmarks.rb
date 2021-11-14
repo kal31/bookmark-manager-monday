50832b80b4161a0d283b017d431af823dfe33938
khalid <khalidmiah@hotmail.co.uk>
2021-11-14 18:56:05 +0000

    adding bookmark using MVC. create method

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -6,4 +6,8 @@ class Bookmark
     result = connection.exec('SELECT * FROM bookmarks')
     result.map { |bookmark| p bookmark['url'] }
   end
+  def self.create(url:)
+      connection = PG.connect(dbname: 'bookmark_manager')
+      connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+  end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,12 +1,24 @@
-feature 'Viewing bookmarks' do
+feature 'Adding a new bookmark' do
+    scenario 'A user can add a bookmark to Bookmark Manager' do
+      visit('/bookmarks/new')
+      fill_in('url', with: 'http://example.org')
+      click_button('Submit')
+  
+      expect(page).to have_content 'http://example.org'
+    end
 
-    # scenario 'A user can see bookmarks' do
-    #     visit('/bookmarks')
-    
-    #     expect(page).to have_content "http://www.makersacademy.com"
-    #     expect(page).to have_content "http://www.destroyallsoftware.com"
-    #     expect(page).to have_content "http://www.google.com"
-    #   end
+    scenario 'Visiting /bookmarks shows me all the bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager')
+      
+           # Add the test data
+      
+      Bookmark.create(url: "http://www.makersacademy.com")
+      Bookmark.create(url: "http://www.destroyallsoftware.com")
+      Bookmark.create(url: "http://www.google.com")
+      
+      visit('/bookmarks')
+    end   
+  end
+  
 
-
-end
+ 
